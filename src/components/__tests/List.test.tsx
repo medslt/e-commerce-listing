4518f86e55fca3ed844e3ec1d09f5895d66a0805
mohamed salamat <moahmed.salamat@gmail.com>
2023-03-11 12:43:48 +0000

    Unit testing: should fetch and return a list of ads

---
+++ src/components/__tests/List.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import List from '../List';
+
+export const DATA = [{
+  id	: 'RANDOM_ID-1',
+  productName: 'RANDOM NAME 123',
+  price: {
+      currencyCode	:	'GBP',
+          priceIncTax	:	189.95,
+          priceExcTax	:	158.29,
+          isOnPromotion	:	false,
+
+  },
+  image: {
+      url: 'IMG_URL',
+      attributes: {
+          imageAltText: 'SOME DESCRIPTION'
+      }
+      
+  },
+  brand: {
+    name: 'Random Brand Name',
+    brandImage: { 
+        url: 'IMG_URL',
+      attributes: {
+          imageAltText: 'SOME DESCRIPTION'
+      }
+    }
+}
+}
+
+]
+
+beforeAll(() => jest.spyOn(window, 'fetch'))
+
+test('renders ads ', async () => {
+  window.fetch.mockResolvedValueOnce({
+    ok: true,
+    json: async () => ({products: DATA}),
+  })
+
+  render(<List />);
+  
+  const adsEl = await screen.findAllByTestId('ad')
+
+  
+  await waitFor(() =>  expect(adsEl).toHaveLength(DATA.length))
+});
