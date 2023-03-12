6980f1a30b53d09a3d2679013f1e870c55b69573
mohamed salamat <moahmed.salamat@gmail.com>
2023-03-12 20:02:49 +0000

    format

--- src/components/Ad/Ad.tsx
+++ src/components/Ad/Ad.tsx
@@ -1,45 +1,51 @@
+import { memo } from "react";
+import { Product } from "src/types";
+import style from "./Ad.module.css";
 
-import style from './Ad.module.css'
+export type AdProps = Product & {
+  testId: string;
+};
 
-import {Product} from 'src/types';
-
-
-export type AdProps = Product  & {
-    testId: string
-}
-
-const currencySymbols: {[key:string]: string} = {
-    'GBP': '£'
-}
+const currencySymbols: { [key: string]: string } = {
+  GBP: "£",
+};
 
 const getFormattedPrice = (currencyCode: string, price: number) => {
-    const currencySymbol = currencySymbols[currencyCode]
-    const priceToDisplay = `${currencySymbol} ${price}`
+  const currencySymbol = currencySymbols[currencyCode];
+  const priceToDisplay = `${currencySymbol} ${price}`;
 
-    return priceToDisplay
-}
+  return priceToDisplay;
+};
 
-const Ad = ({image, productName, price, brand, testId}: AdProps) => {
-    const {url, attributes} = image
-    const {currencyCode, priceIncTax} = price
-    const priceToDisplay = getFormattedPrice(currencyCode, priceIncTax)
+const Ad = ({ image, productName, price, brand, testId }: AdProps) => {
+  const { url, attributes } = image;
+  const { currencyCode, priceIncTax } = price;
+  const priceToDisplay = getFormattedPrice(currencyCode, priceIncTax);
 
-    return (
-
-        <div className={style.ad} data-testid={testId}>
-            <div>
-                <img src={url}  width="270" loading="lazy"  alt={attributes.imageAltText} />
-            </div>
-            <div>
-            <div>
-                <img src={brand.brandImage.url} width="60"  loading="lazy"  alt={brand.brandImage.attributes.imageAltText} />
-            </div>
-                <div> {productName} </div>
-                <div> {priceToDisplay}  </div>
-            </div>
+  return (
+    <div className={style.ad} data-testid={testId}>
+      <div>
+        <img
+          src={url}
+          width="270"
+          loading="lazy"
+          alt={attributes.imageAltText}
+        />
+      </div>
+      <div>
+        <div>
+          <img
+            src={brand.brandImage.url}
+            width="60"
+            loading="lazy"
+            alt={brand.brandImage.attributes.imageAltText}
+          />
         </div>
-    )
-}
+        <div> {productName} </div>
+        <div> {priceToDisplay} </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Ad
+export default memo(Ad);
--- src/components/List/__tests/List.test.tsx
+++ src/components/List/__tests/List.test.tsx
@@ -1,169 +1,171 @@
-import { within, render, screen , waitForElementToBeRemoved} from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import {
+  within,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import List from '../List';
+import List from "../List";
 
-export const PRODUCTS = [{
-    id	: 'RANDOM_ID-1',
-    productName: 'RANDOM NAME 1',
+export const PRODUCTS = [
+  {
+    id: "RANDOM_ID-1",
+    productName: "RANDOM NAME 1",
     price: {
-        currencyCode	:	'GBP',
-            priceIncTax	:	189.95,
-            priceExcTax	:	158.29,
-            isOnPromotion	:	false,
-
+      currencyCode: "GBP",
+      priceIncTax: 189.95,
+      priceExcTax: 158.29,
+      isOnPromotion: false,
     },
     image: {
-        url: 'IMG_URL_1',
-        attributes: {
-            imageAltText: 'IMAGE DESCRIPTION 1'
-        }
-        
+      url: "IMG_URL_1",
+      attributes: {
+        imageAltText: "IMAGE DESCRIPTION 1",
+      },
     },
     brand: {
-      name: 'Random Brand Name 1',
-      brandImage: { 
-          url: 'IMG_URL_1',
+      name: "Random Brand Name 1",
+      brandImage: {
+        url: "IMG_URL_1",
         attributes: {
-            imageAltText: 'BRAND IMAGE DESCRIPTION 1'
-        }
-      }
-  }
-},
-{
-  id	: 'RANDOM_ID-2',
-  productName: 'RANDOM NAME 2',
-  price: {
-      currencyCode	:	'GBP',
-          priceIncTax	:	186.95,
-          priceExcTax	:	159.29,
-          isOnPromotion	:	false,
-
+          imageAltText: "BRAND IMAGE DESCRIPTION 1",
+        },
+      },
+    },
   },
-  image: {
-      url: 'IMG_URL_2',
+  {
+    id: "RANDOM_ID-2",
+    productName: "RANDOM NAME 2",
+    price: {
+      currencyCode: "GBP",
+      priceIncTax: 186.95,
+      priceExcTax: 159.29,
+      isOnPromotion: false,
+    },
+    image: {
+      url: "IMG_URL_2",
       attributes: {
-          imageAltText: 'IMAGE DESCRIPTION 2'
-      }
-      
+        imageAltText: "IMAGE DESCRIPTION 2",
+      },
+    },
+    brand: {
+      name: "Random Brand Name 2",
+      brandImage: {
+        url: "IMG_URL_2",
+        attributes: {
+          imageAltText: "BRAND IMAGE DESCRIPTION 2",
+        },
+      },
+    },
   },
-  brand: {
-    name: 'Random Brand Name 2',
-    brandImage: { 
-        url: 'IMG_URL_2',
-      attributes: {
-          imageAltText: 'BRAND IMAGE DESCRIPTION 2'
-      }
-    }
-}
-}
-
-]
+];
 
 beforeAll(() => {
-  jest.spyOn(window, 'fetch');
-})
+  jest.spyOn(window, "fetch");
+});
 
 beforeEach(() => {
   (window.fetch as jest.Mock).mockResolvedValue({
     ok: true,
-    json: async () => ({products: PRODUCTS}),
+    json: async () => ({ products: PRODUCTS }),
   });
-})
-
-test('should render ads', async () => {
-  
-  render(<List />);
-  
-  const adsEl = await screen.findAllByTestId('ad')
-  
-  expect(adsEl).toHaveLength(PRODUCTS.length)
 });
 
-
-test('should render the Image for each product', async () => {
+test("should render ads", async () => {
   render(<List />);
-  
- await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+  const adsEl = await screen.findAllByTestId("ad");
+
+  expect(adsEl).toHaveLength(PRODUCTS.length);
+});
+
+test("should render the Image for each product", async () => {
+  render(<List />);
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 
   // Check the image for each PRODUCT
-  for(let i=0; i < PRODUCTS.length; i++ ){
-    const currentProduct = PRODUCTS[i]
-    const image = screen.getByRole('img', {
-      name: currentProduct.image.attributes.imageAltText
-    })
+  for (let i = 0; i < PRODUCTS.length; i++) {
+    const currentProduct = PRODUCTS[i];
+    const image = screen.getByRole("img", {
+      name: currentProduct.image.attributes.imageAltText,
+    });
 
-    expect(image).toBeInTheDocument()
+    expect(image).toBeInTheDocument();
   }
 });
 
-test('should render the Product Name for each product', async () => {
+test("should render the Product Name for each product", async () => {
   render(<List />);
-  
-  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 
   // Check the image for each PRODUCT
-  for(let i=0; i < PRODUCTS.length; i++ ){
-    const currentProduct = PRODUCTS[i]
-    const productName = screen.getByText(currentProduct.productName)
+  for (let i = 0; i < PRODUCTS.length; i++) {
+    const currentProduct = PRODUCTS[i];
+    const productName = screen.getByText(currentProduct.productName);
 
-    expect(productName).toBeInTheDocument()
+    expect(productName).toBeInTheDocument();
   }
-})
+});
 
-test('should render the Price for each product', async () => {
+test("should render the Price for each product", async () => {
   render(<List />);
-  
-  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 
   // Check the image for each PRODUCT
-  for(let i=0; i < PRODUCTS.length; i++ ){
-    const currentProduct = PRODUCTS[i]
-    const expectedPrice = '£ ' + currentProduct.price.priceIncTax
+  for (let i = 0; i < PRODUCTS.length; i++) {
+    const currentProduct = PRODUCTS[i];
+    const expectedPrice = "£ " + currentProduct.price.priceIncTax;
 
-    const price = screen.getByText(expectedPrice)
+    const price = screen.getByText(expectedPrice);
 
-    expect(price).toBeInTheDocument()
+    expect(price).toBeInTheDocument();
   }
-})
+});
 
-
-test('should render all Sort BY filter options', async  () => {
+test("should render all Sort BY filter options", async () => {
   render(<List />);
-  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
-  const sortSelect = screen.getByRole('combobox', {
-    name: /sort by:/i
-  })
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+  const sortSelect = screen.getByRole("combobox", {
+    name: /sort by:/i,
+  });
 
-  expect(within(sortSelect).getAllByRole('option').length).toBe(4)
-})
+  expect(within(sortSelect).getAllByRole("option").length).toBe(4);
+});
 
-test('should correctly set default option to Sort BY filter', async () => {
+test("should correctly set default option to Sort BY filter", async () => {
   render(<List />);
-  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
 
-  const sortSelect = screen.getByRole('combobox', {
-    name: /sort by:/i
-  }) as HTMLSelectElement
+  const sortSelect = screen.getByRole("combobox", {
+    name: /sort by:/i,
+  }) as HTMLSelectElement;
 
-  const recommendedOption = within(sortSelect).getByRole('option', { name: 'Recommended' }) as HTMLOptionElement
-  expect(recommendedOption.selected).toBe(true)
-})
+  const recommendedOption = within(sortSelect).getByRole("option", {
+    name: "Recommended",
+  }) as HTMLOptionElement;
+  expect(recommendedOption.selected).toBe(true);
+});
 
-test('should allow user to change Sort_By filter', async () => {
+test("should allow user to change Sort_By filter", async () => {
   render(<List />);
-  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
-  
-  const sortSelect = screen.getByRole('combobox', {
-    name: /sort by:/i
-  })
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+
+  const sortSelect = screen.getByRole("combobox", {
+    name: /sort by:/i,
+  });
 
   userEvent.selectOptions(
     sortSelect,
-    screen.getByRole('option', { name: 'Lowest price' } ),
-  )
+    screen.getByRole("option", { name: "Lowest price" })
+  );
 
-  const LowestPriceOption = await within(sortSelect).findByRole('option', { name: 'Lowest price' }) as HTMLOptionElement
-  
-  expect(LowestPriceOption.selected).toBe(true)
-})
+  const LowestPriceOption = (await within(sortSelect).findByRole("option", {
+    name: "Lowest price",
+  })) as HTMLOptionElement;
+
+  expect(LowestPriceOption.selected).toBe(true);
+});
