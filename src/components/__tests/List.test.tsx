93174f46c46c0545fac867eb74141e21e9bca517
mohamed salamat <moahmed.salamat@gmail.com>
2023-03-12 12:25:17 +0000

    Unit Testing: should display product details (image, price, name)

--- src/components/__tests/List.test.tsx
+++ src/components/__tests/List.test.tsx
@@ -1,30 +1,57 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, within, waitFor, prettyDOM , waitForElementToBeRemoved} from '@testing-library/react';
 import List from '../List';
 
-export const DATA = [{
-  id	: 'RANDOM_ID-1',
-  productName: 'RANDOM NAME 123',
+export const PRODUCTS = [{
+    id	: 'RANDOM_ID-1',
+    productName: 'RANDOM NAME 1',
+    price: {
+        currencyCode	:	'GBP',
+            priceIncTax	:	189.95,
+            priceExcTax	:	158.29,
+            isOnPromotion	:	false,
+
+    },
+    image: {
+        url: 'IMG_URL_1',
+        attributes: {
+            imageAltText: 'IMAGE DESCRIPTION 1'
+        }
+        
+    },
+    brand: {
+      name: 'Random Brand Name 1',
+      brandImage: { 
+          url: 'IMG_URL_1',
+        attributes: {
+            imageAltText: 'BRAND IMAGE DESCRIPTION 1'
+        }
+      }
+  }
+},
+{
+  id	: 'RANDOM_ID-2',
+  productName: 'RANDOM NAME 2',
   price: {
       currencyCode	:	'GBP',
-          priceIncTax	:	189.95,
-          priceExcTax	:	158.29,
+          priceIncTax	:	186.95,
+          priceExcTax	:	159.29,
           isOnPromotion	:	false,
 
   },
   image: {
-      url: 'IMG_URL',
+      url: 'IMG_URL_2',
       attributes: {
-          imageAltText: 'SOME DESCRIPTION'
+          imageAltText: 'IMAGE DESCRIPTION 2'
       }
       
   },
   brand: {
-    name: 'Random Brand Name',
+    name: 'Random Brand Name 2',
     brandImage: { 
-        url: 'IMG_URL',
+        url: 'IMG_URL_2',
       attributes: {
-          imageAltText: 'SOME DESCRIPTION'
+          imageAltText: 'BRAND IMAGE DESCRIPTION 2'
       }
     }
 }
@@ -32,18 +59,69 @@ export const DATA = [{
 
 ]
 
-beforeAll(() => jest.spyOn(window, 'fetch'))
+beforeAll(() => {
+  jest.spyOn(window, 'fetch');
+})
 
-test('renders ads ', async () => {
-  (window.fetch as jest.Mock).mockResolvedValueOnce({
+beforeEach(() => {
+  (window.fetch as jest.Mock).mockResolvedValue({
     ok: true,
-    json: async () => ({products: DATA}),
+    json: async () => ({products: PRODUCTS}),
   });
+})
 
+test('should render ads', async () => {
+  
   render(<List />);
   
   const adsEl = await screen.findAllByTestId('ad')
-
   
-  await waitFor(() =>  expect(adsEl).toHaveLength(DATA.length))
+  expect(adsEl).toHaveLength(PRODUCTS.length)
 });
+
+
+test('should render the Image for each product', async () => {
+  render(<List />);
+  
+ await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+  // Check the image for each PRODUCT
+  for(let i=0; i < PRODUCTS.length; i++ ){
+    const currentProduct = PRODUCTS[i]
+    const image = screen.getByRole('img', {
+      name: currentProduct.image.attributes.imageAltText
+    })
+
+    expect(image).toBeInTheDocument()
+  }
+});
+
+test('should render the Product Name for each product', async () => {
+  render(<List />);
+  
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+  // Check the image for each PRODUCT
+  for(let i=0; i < PRODUCTS.length; i++ ){
+    const currentProduct = PRODUCTS[i]
+    const productName = screen.getByText(currentProduct.productName)
+
+    expect(productName).toBeInTheDocument()
+  }
+})
+
+test('should render the Price for each product', async () => {
+  render(<List />);
+  
+  await waitForElementToBeRemoved(() => screen.queryByText(/loading/i))
+
+  // Check the image for each PRODUCT
+  for(let i=0; i < PRODUCTS.length; i++ ){
+    const currentProduct = PRODUCTS[i]
+    const expectedPrice = '£ ' + currentProduct.price.priceIncTax
+
+    const price = screen.getByText(expectedPrice)
+
+    expect(price).toBeInTheDocument()
+  }
+})
